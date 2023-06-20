cc970aa14e13186df944c64b84092ce8ea2f9f3f
Ani <anikubana@gmail.com>
2023-06-20 13:14:56 +0400

    modified V4 sql file

--- src/main/resources/db/migration/V4__create_recipe_table.sql
+++ src/main/resources/db/migration/V4__create_recipe_table.sql
@@ -1,10 +1,9 @@
 CREATE TABLE Recipes (
      RecipeID INT PRIMARY KEY,
      RecipeName VARCHAR(100) NOT NULL,
-     Instructions TEXT,
-     PreparationTime INT,
      CookingTime INT,
      Servings INT,
      Difficulty VARCHAR(50),
-     Cuisine VARCHAR(50)
+     Cuisine VARCHAR(50),
+     IsPublic BIT(1) NOT NULL
 );
